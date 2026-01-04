e9ae17c4f8c270345af763097ae010bd8549466a
WilliamNgabret <williamngabretchannel@gmail.com>
2026-01-05 01:51:57 +0700

    Delete src directory

--- src/lib/config.ts
+++
@@ -1,8 +0,0 @@
-// Feature flags and configuration
-export const config = {
-  // Set to true when you have a verified Resend domain
-  ENABLE_EMAIL_SENDING: false,
-  
-  // Photo expiry time in hours
-  PHOTO_EXPIRY_HOURS: 24,
-};
